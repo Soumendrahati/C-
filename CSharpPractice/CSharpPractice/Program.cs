13929237d714d0da155c3226ad9bb0c86b50499c
shati <soumendrahati@gmail.com>
2019-02-02 20:42:09 +0530

    C#- 6.0 Static imports

--- CSharpPractice/CSharpPractice/Program.cs
+++ CSharpPractice/CSharpPractice/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Math;
+using static CSharpPractice.Util;
+
 
 namespace CSharpPractice
 {
@@ -10,6 +13,41 @@ namespace CSharpPractice
     {
         static void Main(string[] args)
         {
+            int r = Math.Min(10, 11);
+            Console.WriteLine(r);
+
+            //// To Access the static method above " Static Imports" works
+            //// https://docs.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-6#using-static
+            Console.WriteLine(Me("SH"));
+
+            //// To Access the non static method you have to create object of the class
+            Util util = new Util();
+            Console.WriteLine(util.MeToo("SH"));
+
+            Console.ReadLine();
+
+            
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
---
+++ CSharpPractice/CSharpPractice/Util.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice
+{
+    public class Util
+    {
+        public static string Me(string s)
+        {
+            return "Hello " + s;
+        }
+
+        public string MeToo(string s)
+        {
+            return "HI " + s;
+        }
+    }
+}
