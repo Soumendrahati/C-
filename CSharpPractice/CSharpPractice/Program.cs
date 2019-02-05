ec553c023c221d76162b04e0f5d0ba6878ed3bdc
shati <soumendrahati@gmail.com>
2019-02-05 19:47:00 +0530

    Added some code for encoding

--- CSharpPractice/CSharpPractice/Program.cs
+++ CSharpPractice/CSharpPractice/Program.cs
@@ -13,9 +13,13 @@ namespace CSharpPractice
 {
     class Program
     {
+        public ICollection<Double> grades { get; } = new List<Double>();
         static void Main(string[] args)
         {
-            StaticImports();
+            // StaticImports();
+
+            // Console.WriteLine(UnitTestEmailEncoding());
+            Console.ReadLine();
         }
 
         /// <summary>
@@ -39,5 +43,47 @@ namespace CSharpPractice
             //// The static using directive also imports any nested types
             nested obj = new nested();
         }
-    }
+
+        /// <summary>
+        /// Auto-property initializers
+        //// Auto-property initializers let you declare the initial value 
+        //// for an auto-property as part of the property declaration.
+        /// </summary>
+        public static void AutoPropertyIntializer()
+        {
+           // grades = new List<Double> { 1.2d, 3.0d };
+        }
+
+        public static bool UnitTestEmailEncoding()
+        {
+            //string orderDate = DateTime.Now.ToShortDateString() as string; //1/31/2019
+            DateTime[] dts = new DateTime[] {
+                new DateTime(2019, 01, 01), new DateTime(2019, 01, 30), new DateTime(2019, 02, 01), new DateTime(2019, 02, 28),
+                new DateTime(2019, 12, 01), new DateTime(2019, 12, 30), new DateTime(2019, 03, 01), new DateTime(2019, 03, 30),
+                new DateTime(2019, 04, 01), new DateTime(2019, 04, 30), new DateTime(2019, 05, 01), new DateTime(2019, 05, 30),
+                new DateTime(2019, 06, 01), new DateTime(2019, 06, 30), new DateTime(2019, 07, 01), new DateTime(2019, 07, 30),
+                new DateTime(2019, 08, 01), new DateTime(2019, 08, 30), new DateTime(2019, 09, 01), new DateTime(2019, 09, 30),
+               new DateTime(2019, 10, 01), new DateTime(2019, 10, 30), new DateTime(2019, 11, 01), new DateTime(2019, 11, 30)
+            };
+            int orderNumberN = 0070010655;
+
+            foreach (var date in dts)
+            {
+                //string orderDate = DateTime.Now.ToString("MM/dd/yyyy") as string;
+                string orderDate = date.ToString("MM/dd/yyyy") as string;
+                orderNumberN = orderNumberN + 1; //0081027519
+                string shipToZip = "34608";
+
+                var orderSatusKey = Encoding.UTF8.GetBytes(String.Format("{0}{1}{2}{3}{4}", "00" + orderNumberN.ToString(), ":", shipToZip, ":", orderDate));
+                var orderSatusEmailAuthKey = Convert.ToBase64String(orderSatusKey);
+                Console.WriteLine("00" + orderNumberN.ToString() + ": " + orderSatusEmailAuthKey);
+                if (orderSatusEmailAuthKey.Contains("="))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+}
 }
